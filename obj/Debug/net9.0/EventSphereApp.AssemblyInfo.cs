faf63cbc9ce901debed6dafc5b9bcae25648ed4e
Max <mugunamaxwel@gmail.com>
2025-03-16 23:36:10 +0300

    Updated Pages

--- obj/Debug/net9.0/EventSphereApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventSphereApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventSphereApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+840777f2c4b6a21f09af69d20a9a31d84aacfe88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfb66ff73d7a0bae244f1cf507cd9bd1149f6f16")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventSphereApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventSphereApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
